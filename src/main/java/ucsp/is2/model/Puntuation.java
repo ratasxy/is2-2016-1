b174da62c826fe653161e4abb2f7b311508d4a50
joseline zevallos <joseline.zevallos@ucsp.edu.pe>
2016-05-07 11:50:37 -0500

    puntuacion de cancion

--- src/main/java/ucsp/is2/model/Puntuation.java
+++ src/main/java/ucsp/is2/model/Puntuation.java
@@ -1,12 +1,20 @@
+package ucsp.is2.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+@Entity
+public class Puntuation{
+    @ID
     @SequenceGenerator(name="PUNTIATION_GENERATOR", sequenceName="PUNTUATION_SEQ")
     @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="PUNTUATION_GENERATOR")
 
 
     private integer score = 0;
     public Puntuacion(){
-        
-        
+    
 	}
 	public long getScore() {
 		return score;
@@ -16,9 +24,11 @@
 		this.score = score;
 	}
     
-    public integer clasification(){
+    public static integer clasification(){
         
             score = score + 1;
             Puntuation.save(score);
             System.out.println(score);
     }
+ 
+}
