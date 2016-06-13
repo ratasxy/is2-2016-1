9590e035f7c7e1d2ffdc881ad98ad1edef46f39d
Maria Angela <maria.giraldo@ucsp.edu.pe>
2016-06-13 11:55:39 -0500

    Obtener la cancion deacuerdo a su ID

---
+++ src/main/java/ucsp/is2/rest/ObtainMusicController.java
@@ -0,0 +1,28 @@
+package ucsp.is2.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import ucsp.is2.model.Song;
+
+@RestController
+public class ObtainMusicController {
+	
+	@Autowired
+	private Song songs;
+	
+	@RequestMapping(value="/song/get/json", method=RequestMethod.GET)
+	public ResponseEntity<Song> getSong(@PathVariable("id") long id) {
+		Song song = songs.findById(id);
+		if (song == null) {
+			System.out.println("Song" + id + "not found");
+			return new ResponseEntity<Song>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Song>(song, HttpStatus.OK);	
+	}
+}
