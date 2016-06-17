27825d9228f054aa1b9db98af49b72ef692d2c63
joseline zevallos <joseline.zevallos@ucsp.edu.pe>
2016-06-16 22:20:51 -0500

    Modifique SongRepository y puse todo lo de PuntuationRepository a SongRepository

--- src/main/java/ucsp/is2/repository/SongRepository.java
+++ src/main/java/ucsp/is2/repository/SongRepository.java
@@ -1,10 +1,12 @@
 package ucsp.is2.repository;
 
 import org.springframework.data.repository.Repository;
-
+import antlr.collections.List;
 import ucsp.is2.model.Song;
 
 public interface SongRepository extends Repository<Song, Long> {
 	public Song save(Song song);
 	public Song findById(Long id);
+	List findAll();
+	List findPuntuation (Long puntuation);
 }
