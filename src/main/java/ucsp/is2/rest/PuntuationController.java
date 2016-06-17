2756ce0aca797ac1f0dc65f61c84c9e194b831ee
joseline zevallos <joseline.zevallos@ucsp.edu.pe>
2016-06-16 22:23:06 -0500

    Modifique PuntuationController del #21

--- src/main/java/ucsp/is2/rest/PuntuationController.java
+++ src/main/java/ucsp/is2/rest/PuntuationController.java
@@ -6,23 +6,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import ucsp.is2.model.Puntuation;
+import ucsp.is2.model.Song;
 
 @RestController
 @RequestMapping("/puntuation")
 public class PuntuationController {
 	
 	@Autowired
-	private Puntuation puntuation;
+	private Song puntuation;
 	
 	@RequestMapping(value ="/{id}", method = RequestMethod.GET, produces = "aplication/json")
-	public Puntuation getPuntuation(@PathVariable long score, long id){
+	public Song getPuntuation(@PathVariable long score, long id){
 		puntuation.setId(id);
 		puntuation.setScore(score);
 		return puntuation;		
 	}
 	
-	@RequestMapping(value ="/{id}", method = RequestMethod.GET, produces = "aplication/json")
+	@RequestMapping(value ="/{id}", method = RequestMethod.POST, produces = "aplication/json")
     public void savePuntuation (long id, long score){
     	puntuation.add(0, score +" " +id);
     }
