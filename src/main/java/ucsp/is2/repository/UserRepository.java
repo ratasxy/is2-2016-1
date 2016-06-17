3442df416cfe782c5f8114497bdd03698c6ddc66
carlos chicata farfan <carlos.chicata.farfan@ucsp.edu.pe>
2016-06-17 14:11:44 -0500

    agregando nuevas clase: User, Controller: User y Login

---
+++ src/main/java/ucsp/is2/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package ucsp.is2.repository;
+
+import org.springframework.data.repository.Repository;
+
+import ucsp.is2.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface UserRepository extends Repository< User , Long >{
+	public User save(User song);
+	public User findById(Long id);
+	public List< User > findAll();
+	public Optional< User > findByEmail( String email );
+	public Optional< User > findByPassword( Long id ); /// checkout
+	public void delete( User user );
+}
