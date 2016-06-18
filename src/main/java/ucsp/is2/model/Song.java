40af86f874de0c6e433e5b23da7c669630d3724d
joseline zevallos <joseline.zevallos@ucsp.edu.pe>
2016-06-17 21:48:41 -0500

    Fixed identation #21

--- src/main/java/ucsp/is2/model/Song.java
+++ src/main/java/ucsp/is2/model/Song.java
@@ -8,48 +8,37 @@ import javax.persistence.SequenceGenerator;
 
 @Entity
 public class Song {
-	
 	@Id
 	@SequenceGenerator(name="SONG_ID_GENERATOR", sequenceName="SONG_ID_SEQ")
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SONG_ID_GENERATOR")
 	private long id;
-	public long Play_Counter=0;
-    private long score = 0;
+	public Long Play_Counter;
+	private Long score;
 	private String name;
 	
 	public Song (long score){
-    	this.score =score;
-    }
-	
+		this.score =score;
+	}
 	public void Increase_Counter(){
 		Play_Counter++;
 	}
-
 	public long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-	
 	public long getScore() {
-  		return score;
-  	}
-
-  	public void setScore(long score) {
-  		this.score = score;
-  	}
-
-	public void add(int i, String name) {
-		//
+		return score;
 	}
+	public void setScore(Long score) {
+		this.score = score;
+	}
+	public void add(Integer i,String name) {}
 }
