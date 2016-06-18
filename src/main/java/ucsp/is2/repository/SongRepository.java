296ca20f6b03bb9657929452633459b72440b6ef
joseline zevallos <joseline.zevallos@ucsp.edu.pe>
2016-06-17 21:49:35 -0500

    Fixed identation, SongRepository #21

--- src/main/java/ucsp/is2/repository/SongRepository.java
+++ src/main/java/ucsp/is2/repository/SongRepository.java
@@ -4,9 +4,8 @@ import org.springframework.data.repository.Repository;
 import antlr.collections.List;
 import ucsp.is2.model.Song;
 
-public interface SongRepository extends Repository<Song, Long> {
+public interface SongRepository extends Repository<Song,Long> {
 	public Song save(Song song);
 	public Song findById(Long id);
-	List findAll();
 	List findPuntuation (Long puntuation);
 }
