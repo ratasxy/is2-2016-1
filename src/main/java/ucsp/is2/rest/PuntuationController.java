959db445d96df5be1601e2867b4cc3fff8c73612
joseline zevallos <joseline.zevallos@ucsp.edu.pe>
2016-06-17 21:50:11 -0500

    Fixed identation, PuntuationController #21

--- src/main/java/ucsp/is2/rest/PuntuationController.java
+++ src/main/java/ucsp/is2/rest/PuntuationController.java
@@ -5,25 +5,22 @@ import org.springframework.stereotype.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import ucsp.is2.model.Song;
 
 @RestController
 @RequestMapping("/puntuation")
 public class PuntuationController {
-	
 	@Autowired
 	private Song puntuation;
-	
+
 	@RequestMapping(value ="/{id}", method = RequestMethod.GET, produces = "aplication/json")
-	public Song getPuntuation(@PathVariable long score, long id){
+	public Song getPuntuation(@PathVariable Long score,Long id){
 		puntuation.setId(id);
 		puntuation.setScore(score);
-		return puntuation;		
+		return puntuation;
 	}
-	
 	@RequestMapping(value ="/{id}", method = RequestMethod.POST, produces = "aplication/json")
-    public void savePuntuation (long id, long score){
-    	puntuation.add(0, score +" " +id);
-    }
+	public void savePuntuation (Long id,Long score){
+		puntuation.add(0,score+" "+id);
+	}
 }
